3d80a7ecad4dfb547c0573994346961ba893121c
titanXL <emildavidkov88@gmail.com>
2020-05-01 10:21:43 +0300

    Safe area view for acc screen

--- src/screens/AccountScreen/index.tsx
+++ src/screens/AccountScreen/index.tsx
@@ -3,18 +3,19 @@ import { Text, View, StyleSheet } from "react-native";
 import { Button } from "react-native-elements";
 import { Context as AuthContet } from "../../context/AuthContext";
 import { Spacer } from "../../components/Spacer";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 interface IProps {}
 
 export const AccountScreen: React.FC<IProps> = ({}) => {
   const { signOut } = useContext(AuthContet);
   return (
-    <View>
+    <SafeAreaView>
       <Text>Account Screen</Text>
       <Spacer>
         <Button title="Sign Out" onPress={signOut} />
       </Spacer>
-    </View>
+    </SafeAreaView>
   );
 };
 const styles = StyleSheet.create({});
